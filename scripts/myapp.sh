7c8b3a1740eeb5d4610f5e640bfb96ba89cc3bb6
aniket <aniketlagad6@gmail.com>
2023-10-20 10:49:15 +0530

    new  line addded

--- scripts/myapp.sh
+++ scripts/myapp.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page296/oxer.zip
-sudo unzip oxer.zip -d /var/www/html/
+# #!/bin/bash
+# sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page296/oxer.zip
+# sudo unzip oxer.zip -d /var/www/html/
